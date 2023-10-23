de93c96ae4ac1879e2d92e4c9f1779ef44b0a735
Manrojw <manevichrr@yandex.ru>
2023-10-23 06:24:22 -0400

    Added status codes constants

---
+++ src/test/java/ru/yandex/constants/StatusCode.java
@@ -0,0 +1,12 @@
+package ru.yandex.constants;
+
+    public class StatusCode {
+
+        public static final int CODE_200 = 200;
+        public static final int CODE_201 = 201;
+        public static final int CODE_400 = 400;
+        public static final int CODE_404 = 404;
+        public static final int CODE_409 = 409;
+
+    }
+
